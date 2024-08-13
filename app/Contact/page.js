d4704517fb6760326bc3da74ea55979b12c7a6ac
rahuldodwe <rahuldodwe7747909605@gmail.com>
2024-08-13 16:19:23 +0530

    Commit

---
+++ app/Contact/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+      <h1 className='text-xl font-bold'>This is Contact Page</h1>
+      <a className='bg-cyan-400 p-1 rounded-md' href="/">Home Page</a>
+    </div>
+  )
+}
+
+export default Contact
--- app/page.js
+++ app/page.js
@@ -1,18 +1,11 @@
-"use client"
-import React, { useState } from 'react'
+import React from 'react'
 
-const page = () => {
-  const [username, setusername] = useState("")
+function page() {
   return (
-    <>
-    <h1 className='mx-5 text-xl'>Enter Your Name</h1>
-    <form>
-      <input type="text" className='border-2 border-zinc-800 px-4 py-2 text-x1' value={username} onChange={(e)=>{
-        setusername(e.target.value)
-        console.log(username)
-      }} />
-    </form>
-    </>
+    <div>
+      <h1 className='text-xl font-bold'>This is Home Page</h1>
+      <a className='bg-cyan-400 p-1 rounded-md' href="/Contact"> Contact Page</a>
+    </div>
   )
 }
 
